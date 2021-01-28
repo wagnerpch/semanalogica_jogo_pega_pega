8b3a2d66c0ecdca47915d6097cb8ec1d41bc1a6e
Wagner Chequeleiro Cordeiro <w.chequeleiro@gmail.com>
2021-01-27 22:00:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# semanalogica_jogo_pega_pega
+ Jogo de pega pega aprendido durante o Evento sobre Lógica de Programação
---
+++ app.js
@@ -0,0 +1,21 @@
+// Pega-pega
+// Criar o botão (  )
+// Locia que faça o botão correr (  )
+// Logica quando ganha o jogo (  )
+// variáveis -> number (positivo, negativo, inteiro, real), string (qualquer frase letra, acentuação; usando ['valores'] ou ["valores"] ou [`valores`]), boolean (true, false)-> primário
+// function nomeDaFuncao(Valores, ...) {
+// executa algo e pode ou não retornas algum valor    
+// }
+// 
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener('mouseover', () => {
+    btn.style.position = "absolute";
+    btn.style.top = `${(Math.floor(Math.random() * (window.innerHeight - 18))}px`;
+    btn.style.left = `${(Math.floor(Math.random() * (window.innerHeight - 18))}px`;
+})
+
+btn.addEventListener('click', ( => {
+    console.log('Você ganhou')
+})
